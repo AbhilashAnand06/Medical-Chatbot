5a6caf43aa14171154073672f406077f9a35a96d
AbhilashAnand06 <abhilash.anand0695@gmail.com>
2025-09-14 23:01:10 +0200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating necessary directories for the Medical-Chatbot project
+mkdir -p Medical-Chatbot/src
+mkdir -p Medical-Chatbot/research
+
+# Creating files
+touch Medical-Chatbot/src/__init__.py
+touch Medical-Chatbot/src/helper.py
+touch Medical-Chatbot/src/prompt.py
+touch Medical-Chatbot/.env
+touch Medical-Chatbot/setup.py
+touch Medical-Chatbot/app.py
+touch Medical-Chatbot/research/trials.ipynb
+touch Medical-Chatbot/requirements.txt
+
+echo "Directory structure for Medical-Chatbot created successfully!"
