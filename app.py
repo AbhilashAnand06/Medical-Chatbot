10484233207c8f53cc4bae858b23a7c5b00ecd56
AbhilashAnand06 <abhilash.anand0695@gmail.com>
2025-09-19 01:49:02 +0200

    updated host in app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ async def chat(msg: str = Form(...)):
 # Run server
 # ----------------------
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
